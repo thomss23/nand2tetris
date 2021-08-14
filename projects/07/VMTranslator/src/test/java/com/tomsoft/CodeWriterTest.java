1ee98c4282c8421a5e8f5cb209e43e5b64d1ebe5
Thomas Borbeli <borbelithomas@gmail.com>
2021-08-14 13:32:33 +0300

    Add Project 7 VM translator

---
+++ projects/07/VMTranslator/src/test/java/com/tomsoft/CodeWriterTest.java
@@ -0,0 +1,18 @@
+package com.tomsoft;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class CodeWriterTest {
+
+    @org.junit.jupiter.api.Test
+    void writeArithmetic() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void writePushPop() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void close() {
+    }
+}
